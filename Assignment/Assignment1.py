4b3d1cff3c0c21d9c4699aa1ace86695be4edeb0
DEXTERcyy <dextercyy@gmail.com>
2023-11-05 00:53:42 +0100

    Question3

--- Assignment/Assignment1.py
+++ Assignment/Assignment1.py
@@ -14,22 +14,22 @@ class Plus():
 
 class Sigmoid():
 
-    def __init__(self):
-        pass
+  def __init__(self):
+    self.sigx = None
 
-    def forward(self, x):
-        sigx = 1 / (1 + np.exp(-x))
-        self.sigx = sigx
-        return sigx
+  def forward(self, x):
+    sigx = 1 / (1 + np.exp(-x))
+    self.sigx = sigx
+    return sigx
 
-    def backward(self, goutput):
-        sigx = self.sigx
-        return goutput * sigx * (1 - sigx)
+  def backward(self, goutput):
+    sigx = self.sigx
+    return goutput * sigx * (1 - sigx)
 
 class Softmax():
 
     def __init__(self):
-        pass
+        self.sofx = None
 
     def forward(self, x):
         sofx = [math.exp(i) / sum([math.exp(j) for j in x]) for i in x]
@@ -37,62 +37,81 @@ class Softmax():
         return sofx
 
     def backward(self, soutput):
-        sofx = self.sofx
         gradients = []
+        sofx = self.sofx
         for i in range(len(sofx)):
-            gradient = sofx[i] * (1 - sofx[i])
-            if i == soutput:
-                gradient -= 1.0
-            gradients.append(gradient)
-        return gradients
+          if t[i] == 1:
+            gradients.append(sofx[i]*(1-sofx[i]))
+          else:
+            gradients.append(-sofx[0]*sofx[i])
+        return np.array(gradients)*soutput
 
 class RowSum():
 
     def __init__(self):
-        pass
+      pass
 
     def forward(self, x):
-        sumd = x.sum(axis=1)
-        self.x = x.shape[1]
-        return sumd
+      x = np.array(x)
+      sumd = x.sum(axis=1)
+      self.x = x.shape[1]
+      return sumd
 
     def backward(self, gy):
-        n, m= gy.shape[0], self.x
-        return gy[:,None].expand(n, m)
+      gy = np.array(gy)
+      return np.broadcast_to(gy[:, None], (gy.shape[0], self.x))
 
 class Expand():
 
     def __init__(self):
         pass
 
-    def forward(self, x, size):
-        return np.full(x, size=size)
+    def forward(self, size, x):
+        return np.full(size, x)
 
     def backward(self, gy):
-        return gy.sum(), None
+      gy = np.array(gy)
+      return gy.sum(), None
 
 # Input layer
-inputs = [1.,-1.]
+inputs = np.array([1.,-1.])
 
 # First layer
-W = [[1., 1., 1.], [-1., -1., -1.]]
-b = [0., 0., 0.]
+W = np.array([[1., 1., 1.], [-1., -1., -1.]])
+b = np.array([0., 0., 0.])
 
 # Second layer
-V = [[1., 1.], [-1., -1.], [-1., -1.]]
-c = [0., 0.]
+V = np.array([[1., 1.], [-1., -1.], [-1., -1.]])
+c = np.array([0., 0.])
 
-# Sigmoid activation function
+# true class
+t=np.array([1,0])
+
+#activation function
 sigmoid = Sigmoid()
-
-# Softmax activation function
 softmax = Softmax()
+rowsum = RowSum()
+expand = Expand()
 
 # Forward pass
 k = np.dot(inputs, W) + b
 h = sigmoid.forward(k)
 s = np.dot(h, V) + c
 o = softmax.forward(s)
-loss = -np.log(o[1])
+
+# Cross-entropy loss derivative for correct class
+l = -np.log(o[0])
+dl = -1/o[0] #dl/do [-2]
 
 # Backward pass
+ds = np.array(softmax.backward(dl)) #dl/ds c
+dh = np.dot(ds, np.array(V).T) #dl/dh b
+dk = sigmoid.backward(dh)# dl/dk 
+dw = np.dot(np.array([inputs]).T, np.array([dk])) #dk/dw W
+dv = np.dot(np.array([h]).T, np.array([ds]))
+
+# Print the derivatives
+print("W:", dw)
+print("b", dh)
+print("V", dv)
+print("c", ds)
